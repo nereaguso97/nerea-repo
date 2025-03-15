1fe64c51a17298a6030f2e4bce75e3257db40fe8
Nerea Gutierrez <gutierrezsotomayornerea@gmail.com>
2025-03-15 17:08:58 +0100

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,4 +1,4 @@
 nombre = "lucas hola que tal modifico para fetch. Otra vez y luquitas luquitas"
-apellido = "Dalto cambio prueba pull para fetch"
+apellido = "Dalto cambio prueba pull para fetch lucas"
 
 print (nombre + apellido + "¿Cómo estás?")
